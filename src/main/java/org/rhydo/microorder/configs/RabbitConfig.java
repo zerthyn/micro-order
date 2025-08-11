1a68120bedb0528694d1ad6a9a1d0d06fc7fe7d7
zerthyn <zerthyn7@gmail.com>
2025-08-11 08:47:42 +0800

    Config RabbitMQ

---
+++ src/main/java/org/rhydo/microorder/configs/RabbitConfig.java
@@ -0,0 +1,62 @@
+package org.rhydo.microorder.configs;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.routing.name}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder.durable(queueName).build();
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return ExchangeBuilder.topicExchange(exchangeName)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
+        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
+        rabbitAdmin.setAutoStartup(true);
+        return rabbitAdmin;
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate temlate = new RabbitTemplate(connectionFactory);
+        temlate.setMessageConverter(messageConverter());
+        temlate.setExchange(exchangeName);
+        return temlate;
+    }
+}
